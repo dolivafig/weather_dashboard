21c450f2045b426d1af28906a736900918c9aff8
gman <dolivafigueroa@gmail.com>
2023-02-08 13:54:44 -0500

    add btn event cliker to save local

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,19 @@
 var APIkey = "d2293f90cfd082b6965ab5c95c4fbe70";
 var lat;
 var lon;
+var cityInput = document.querySelector("#cityInput");
+var submitbtn = document.querySelector("#searchbtn");
+var cityList = document.querySelector(".cityList");
 // https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={d2293f90cfd082b6965ab5c95c4fbe70}
 
-
+submitbtn.addEventListener("click" , function(event) {
+var city = cityInput.value;
+localStorage.setItem("city", city);
+var citybtn = document.createElement("button");
+cityList.appendChild(citybtn);
+cityList.setAttribute("type", "button;");
+getLongLat(city);
+})
 
 function getLongLat(city) {
     fetch('http://api.openweathermap.org/geo/1.0/direct?q='+ city +'&limit=1&appid='+ APIkey, {
@@ -31,10 +41,13 @@ function getWeather(lat, lon) {
     })
         .then(function (data) {
             console.log(data);
+            console.log(data.city.name);
+            console.log(data.list[0]);
+            console.log(data.list[0].dt_text);
+            console.log(data.list[0].weather[2]);
         });
         // displayWeather(data);
 }
 
 // function displayWeather( )
 
-getLongLat('Miami');
